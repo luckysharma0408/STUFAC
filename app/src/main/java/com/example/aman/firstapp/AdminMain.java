33d3383092f7ae6ce02a75bad77afc0a029c68f6
Lucky <sharma1999lucky@gmail.com>
2019-11-14 00:06:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aman/firstapp/AdminMain.java
@@ -0,0 +1,50 @@
+package com.example.aman.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AdminMain extends AppCompatActivity {
+
+    private ImageView create;
+    private ImageView view;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+
+        create = (ImageView) findViewById(R.id.imageView9);
+        view = (ImageView) findViewById(R.id.imageView10);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminMain.this,create.class);
+                startActivity(intent);
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inte = new Intent(AdminMain.this,view.class);
+                startActivity(inte);
+            }
+        });
+
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.admin_menu,menu);
+        return true;
+    }
+}
